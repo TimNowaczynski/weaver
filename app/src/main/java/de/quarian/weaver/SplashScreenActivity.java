3252b701b0cd4dc6125b8c20379d0688cc7834fc
Tim Nowaczynski <timnowaczynski@quarian.de>
2019-12-31 05:54:55 +0100

    Splash Screen now has a dummy delay of one second before navigating to the campaign screen.

--- app/src/main/java/de/quarian/weaver/SplashScreenActivity.java
+++ app/src/main/java/de/quarian/weaver/SplashScreenActivity.java
@@ -1,8 +1,12 @@
 package de.quarian.weaver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import de.quarian.weaver.campaigns.CampaignListActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,5 +16,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final Context applicationContext = getApplicationContext();
+        final Intent intent = new Intent(applicationContext, CampaignListActivity.class);
+
+        // Start with a delay to show the splash screen. Later on we could do startup
+        // preparations if needed as well.
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> startActivity(intent), 1000);
     }
 }
---
+++ app/src/main/java/de/quarian/weaver/campaigns/CampaignListActivity.java
@@ -0,0 +1,16 @@
+package de.quarian.weaver.campaigns;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import de.quarian.weaver.R;
+
+public class CampaignListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_list);
+    }
+}
