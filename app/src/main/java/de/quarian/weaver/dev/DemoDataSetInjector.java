3ca7dd68a84a1e9d6ad4b0f14b194e625e597f0b
Tim Nowaczynski <timnowaczynski@quarian.de>
2020-02-03 11:10:23 +0100

    Added DSA campaign to demo data-set

--- app/src/main/java/de/quarian/weaver/dev/DemoDataSetInjector.java
+++ app/src/main/java/de/quarian/weaver/dev/DemoDataSetInjector.java
@@ -49,9 +49,15 @@ public class DemoDataSetInjector {
 
         final RoleplayingSystem sr = new RoleplayingSystem();
         sr.roleplayingSystemName = "Shadowrun";
-        sr.logoImage = convertDrawableResToBytes(R.drawable.sr_dice);
+        sr.logoImage = convertDrawableResToBytes(R.drawable.shadowrun_logo);
         sr.logoImageType = "image/jpeg";
         ids[0] = roleplayingSystemDAO.createRoleplayingSystem(sr);
+
+        final RoleplayingSystem dsa = new RoleplayingSystem();
+        dsa.roleplayingSystemName = "DSA 4.0";
+        dsa.logoImage = convertDrawableResToBytes(R.drawable.dsa_logo);
+        dsa.logoImageType = "image/jpeg";
+        ids[1] = roleplayingSystemDAO.createRoleplayingSystem(dsa);
 
         return ids;
     }
@@ -61,11 +67,15 @@ public class DemoDataSetInjector {
         final long[] themeIDs = new long[3];
 
         final Theme srTheme = new Theme();
-
         srTheme.bannerBackgroundImage = convertDrawableResToBytes(R.drawable.shadowrun_banner);
         srTheme.bannerBackgroundImageType = "image/jpeg";
 
+        final Theme dsaTheme = new Theme();
+        dsaTheme.bannerBackgroundImage = convertDrawableResToBytes(R.drawable.g7_banner);
+        dsaTheme.bannerBackgroundImageType = "image/jpeg";
+
         themeIDs[0] = themeDAO.createTheme(srTheme);
+        themeIDs[1] = themeDAO.createTheme(dsaTheme);
         //TODO: define theme values
         return themeIDs;
     }
@@ -74,6 +84,7 @@ public class DemoDataSetInjector {
         final CampaignDAO campaignDAO = weaverDB.campaignDAO();
         final long[] campaignIds = new long[3];
         final Date today = new Date();
+
         final Date fiveMinutesAgo = new Date(today.getTime() - 1000L * 60L * 5L);
         final Date oneHourAgo = new Date(today.getTime() - 1000L * 60L * 60L);
         final Date yesterday = new Date(today.getTime() - 1000L * 60L * 60L * 24L);
@@ -91,6 +102,26 @@ public class DemoDataSetInjector {
                 "after taking a long rest. The question is now, which side to take, to somehow " +
                 "avoid getting struck down in the emerging power struggle.";
         campaignIds[0] = campaignDAO.createCampaign(risingDragon);
+
+        final Date fourteenMonthAgo = new Date(today.getTime() - 1000L * 60L * 60L * 24L * 30L * 14L + 1000L * 60L * 60L * 24L * 5L);
+        final Date twelveDaysAgo = new Date(today.getTime() - 1000L * 60L * 60L * 12L);
+        final Date aWeekAgo = new Date(today.getTime() - 1000L * 60L * 60L * 7L);
+
+        final Campaign borbarad = new Campaign();
+        borbarad.archived = false;
+        borbarad.roleplayingSystemId = roleplayingSystemIDs[1];
+        borbarad.themeId = themeIds[1];
+        borbarad.creationDateMillis = fourteenMonthAgo.getTime();
+        borbarad.editDateMillis = twelveDaysAgo.getTime();
+        borbarad.lastUsedDataMillis = aWeekAgo.getTime();
+        borbarad.campaignName = "Die 7 Gezeichneten";
+        borbarad.synopsis = "Things are usually rather relaxed during the festive days in early " +
+                "summer. But there are powers struggling to bring back an ancient sorcerer who " +
+                "is hungry for power and won't stop to sacrifice anything in his way. So the " +
+                "burden of stopping him falls upon an unlucky group of adventurers, chosen by " +
+                "manifestations of certain marks of old times which are neither magical or holy " +
+                "by nature but will grant them otherworldly powers so far unknown.";
+        campaignIds[1] = campaignDAO.createCampaign(borbarad);
         return campaignIds;
     }
 
