646591c69e9ca765deb8f4f2b68e74fa88d5bf56
Tim Nowaczynski <timnowaczynski@quarian.de>
2020-02-04 21:02:53 +0100

    Campaign Title is now set as Activity Title in Character Library Activity

--- app/src/main/java/de/quarian/weaver/characters/CharacterLibraryActivity.java
+++ app/src/main/java/de/quarian/weaver/characters/CharacterLibraryActivity.java
@@ -1,25 +1,54 @@
 package de.quarian.weaver.characters;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+
+import javax.inject.Inject;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import de.quarian.weaver.NavigationController;
 import de.quarian.weaver.R;
 import de.quarian.weaver.RequestCodes;
+import de.quarian.weaver.database.CampaignDAO;
+import de.quarian.weaver.database.WeaverDB;
+import de.quarian.weaver.datamodel.Campaign;
+import de.quarian.weaver.di.ApplicationModule;
+import de.quarian.weaver.di.DaggerApplicationComponent;
 
 public class CharacterLibraryActivity extends AppCompatActivity {
 
     public static String EXTRA_CAMPAIGN_ID = "extra.campaignId";
+
+    @Inject
+    public WeaverDB weaverDB;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_library);
         setTitle(R.string.activity_title_character_library);
-        //TODO: Override title with campaign name
+        injectDependencies();
+
+        final long campaignId = getIntent().getLongExtra(EXTRA_CAMPAIGN_ID, -1L);
+        if (campaignId < 0) {
+            throw new IllegalArgumentException("Campaign ID not provided in extras");
+        }
+
+        AsyncTask.execute(() -> {
+            final CampaignDAO campaignDAO = weaverDB.campaignDAO();
+            final Campaign campaign = campaignDAO.readCampaignByID(campaignId);
+            runOnUiThread(() -> setTitle(campaign.campaignName));
+        });
+    }
+
+    private void injectDependencies() {
+        DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(getApplicationContext()))
+                .build()
+                .inject(this);
     }
 
     // Listeners
--- app/src/main/java/de/quarian/weaver/di/ApplicationComponent.java
+++ app/src/main/java/de/quarian/weaver/di/ApplicationComponent.java
@@ -5,6 +5,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 import de.quarian.weaver.WeaverApplication;
 import de.quarian.weaver.campaigns.CampaignListActivity;
+import de.quarian.weaver.characters.CharacterLibraryActivity;
 import de.quarian.weaver.dev.DeveloperFunctionsActivity;
 import de.quarian.weaver.theming.SetThemeActivity;
 import de.quarian.weaver.theming.WeaverThemedActivity;
@@ -19,6 +20,7 @@ public interface ApplicationComponent {
     void inject(final WeaverApplication weaverApplication);
     void inject(final WeaverThemedActivity  weaverThemedActivity);
     void inject(final CampaignListActivity campaignListActivity);
+    void inject(final CharacterLibraryActivity characterLibraryActivity);
     void inject(final SetThemeActivity setThemeActivity);
     void inject(final DeveloperFunctionsActivity developerFunctionsActivity);
 }
